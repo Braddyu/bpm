0065713c17f9f04b0fb6673193a5e76f4e100944
wangshaoqi <574040736@qq.com>
2018-02-08 10:55:14 +0800

    update

--- code/bpm/app/project/bpm_resource/services/node_transfer_service.js
+++ code/bpm/app/project/bpm_resource/services/node_transfer_service.js
@@ -576,8 +576,8 @@ async function normal_process(current_detail,next_detail, next_node, proc_inst_i
     data_original.proc_back = 0;
     var update_original={$set:data_original}
     await model.$ProcessInstTask.update(conditions_original, update_original, options);
-    let rest=await touchNode(current_detail,user_code,proc_inst_task_id,false);
-    if(rest.success){resolve(rest);return ;}
+    // let rest=await touchNode(current_detail,user_code,proc_inst_task_id,false);
+    // if(rest.success){resolve(rest);return ;}
     let r=await model.$ProcessInstTask.find({'_id':proc_inst_task_id});
     if(!r.length){NoFound(resolve);return ;}
     var obj=new Object(r[0]._doc)
