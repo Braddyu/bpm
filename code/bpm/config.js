45da3b38f2eb2b02c74fe4544cb0688796a456b1
huyingjiang <1670607823@qq.com>
2018-04-16 10:41:12 +0800

    修改帮助文档和常见问题bug
    Signed-off-by: huyingjiang <1670607823@qq.com>

--- code/bpm/config.js
+++ code/bpm/config.js
@@ -292,5 +292,6 @@ var config = {
     local_haunghe_path:'E:/test/',//本地存储黄河数据
     local_path:'/upload/',//本地存储附件路径
     batch_size:2000,//差错工单每一批次数量
+	accessory_path:'C:/res_download_file/' //帮助资料
 }
 module.exports = config;
