8bd55eff546ed05e3c9daaf06bfe530a8a1991f4
wangshaoqi <574040736@qq.com>
2018-04-17 09:27:51 +0800

    update

--- code/bpm/app/project/bpm_resource/services/node_analysis_service.js
+++ code/bpm/app/project/bpm_resource/services/node_analysis_service.js
@@ -51,7 +51,7 @@ function getNode(process_define_id,node_code,params,flag){
             let type = node_detail.type;
             var node_array = await getValidNode(process_define, node_code, flag);
             if (type == "chat") {
-                var valid_node = await deleteInvalidNode(process_define, item_config, node_array, node_code, params,)
+                var valid_node = await deleteInvalidNode(process_define, item_config, node_array, node_code, params)
                 if (valid_node.length != 1) {
                     resolve(utils.returnMsg(false, '9999', '有效节点删除不完全，或者错误', valid_node, null));
                 } else {
@@ -103,7 +103,7 @@ function getNextnode(inst_id,node_code,params,flag){
             let type = node_detail.type;
             var node_array = await getValidNode(process_define, node_code, flag);
             if (type == "chat") {
-                var valid_node = await deleteInvalidNode(process_define, item_config, node_array, node_code, params,)
+                var valid_node = await deleteInvalidNode(process_define, item_config, node_array, node_code, params)
                 if (valid_node.length != 1) {
                     resolve(utils.returnMsg(false, '9999', '有效节点删除不完全，或者错误', valid_node, null));
                 } else {
@@ -149,7 +149,7 @@ function getInstNode(rs,node_code,params,flag){
             let type = node_detail.type;
             var node_array = await getValidNode(process_define, node_code, flag);
             if (type == "chat") {
-                var valid_node = await deleteInvalidNode(process_define, item_config, node_array, node_code, params,)
+                var valid_node = await deleteInvalidNode(process_define, item_config, node_array, node_code, params)
                 if (valid_node.length != 1) {
                     resolve(utils.returnMsg(false, '9999', '有效节点删除不完全，或者错误', valid_node, null));
                 } else {
